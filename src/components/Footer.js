750c7d4eb5aaba68a3b8ba97887079b0781f6f8f
01clarian <clarionnorth@gmail.com>
2025-03-06 11:38:08 -0500

    nav updates ra

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,8 @@ import { SocialIcon } from "react-social-icons";
 export default function Footer() {
   return (
     <div>
-            <div style={{ textAlign: "center" }}>
+      {/* ✅ Get Listed Button */}
+      <div style={{ textAlign: "center" }}>
         <a
           className="example_e"
           href="https://visitor.r20.constantcontact.com/d.jsp?llr=csi9ozbab&p=oi&m=csi9ozbab&sit=zaxq5c9bb&f=56937576-3074-4bab-becd-a2b8ce970e8b"
@@ -19,19 +20,47 @@ export default function Footer() {
         </a>
       </div>
 
+      {/* ✅ Social Icons */}
       <h3 className="taCenter" style={{ margin: "0" }}>
         <br />
         <div>
-          {["https://www.facebook.com/listedbookings", 
-            "https://www.instagram.com/areulisted", 
-            "https://twitter.com/areulisted", 
-            "https://soundcloud.com/areulisted"].map((url, index) => (
+          {/* ✅ Existing Social Icons */}
+          {[
+            "https://www.facebook.com/listedbookings",
+            "https://www.instagram.com/areulisted",
+            "https://twitter.com/areulisted",
+            "https://soundcloud.com/areulisted",
+          ].map((url, index) => (
             <span key={index} style={{ margin: "5px" }}>
               <SocialIcon url={url} />
             </span>
           ))}
+
+          {/* ✅ Custom RA Icon */}
+          <span style={{ margin: "5px" }}>
+            <a
+              href="https://ra.co/promoters/91"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/ra.png"
+                alt="RA"
+                style={{
+                  marginBottom:"-20px",
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "50%",
+                  objectFit: "contain",
+                  filter: "invert(1)", // Makes it white if dark background
+                }}
+              />
+            </a>
+          </span>
         </div>
       </h3>
+
+      {/* ✅ Footer */}
       <footer className="footer">
         <div className="container taCenter">
           <span style={{ color: "white" }}>
