cbe07346926d45a55c76367d22478425917563d0
01clarian <clarionnorth@gmail.com>
2025-03-06 11:43:59 -0500

    icons for nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -32,24 +32,64 @@ export default function Navigation() {
         <Link href="/home/" onClick={handleLinkClick}>
           <Logo />
         </Link>
+
+        {/* ✅ Social Icons including RA */}
         <div className="smedialinks">
-          {["https://www.facebook.com/listedbookings", 
-            "https://www.instagram.com/areulisted", 
-            "https://twitter.com/areulisted", 
-            "https://soundcloud.com/areulisted"].map((url, index) => (
+          {[
+            "https://www.facebook.com/listedbookings",
+            "https://www.instagram.com/areulisted",
+            "https://twitter.com/areulisted",
+            "https://soundcloud.com/areulisted",
+          ].map((url, index) => (
             <span key={index} style={{ margin: "5px" }}>
               <SocialIcon url={url} style={{ height: 25, width: 25 }} />
             </span>
           ))}
+
+          {/* ✅ Custom RA Icon */}
+          <span style={{ margin: "5px" }}>
+            <a
+              href="https://ra.co/promoters/91"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src="/ra.png"
+                alt="RA"
+                style={{
+                  marginBottom:"-8.3px",
+                  width: "25px",
+                  height: "25px",
+                  borderRadius: "50%",
+                  objectFit: "contain",
+                  filter: "invert(1)", // Ensures visibility on dark backgrounds
+                }}
+              />
+            </a>
+          </span>
         </div>
 
         {/* ✅ Navigation Links */}
         <div className="Nav--Links">
-          <Link id="navc1" href="/home/" className={`NavLink ${currentPath === "/home/" ? "active" : ""}`} onClick={handleLinkClick}>Home</Link>
-          
+          <Link
+            id="navc1"
+            href="/home/"
+            className={`NavLink ${currentPath === "/home/" ? "active" : ""}`}
+            onClick={handleLinkClick}
+          >
+            Home
+          </Link>
+
           {/* ✅ Artists Dropdown */}
           <div className="dropmenu">
-            <Link id="navc2" href="/artists/" className={`NavLink ${currentPath.startsWith("/artists") ? "active" : ""}`} onClick={handleLinkClick}>
+            <Link
+              id="navc2"
+              href="/artists/"
+              className={`NavLink ${
+                currentPath.startsWith("/artists") ? "active" : ""
+              }`}
+              onClick={handleLinkClick}
+            >
               Artists
             </Link>
             <ul>
@@ -67,10 +107,33 @@ export default function Navigation() {
             </ul>
           </div>
 
-          <Link id="navc3" href="/productions/" className={`NavLink ${currentPath === "/productions/" ? "active" : ""}`} onClick={handleLinkClick}>Productions</Link>
-          <Link id="navc4" href="/buzz/" className={`NavLink ${currentPath === "/news/" ? "active" : ""}`} onClick={handleLinkClick}>Buzz</Link>
-          <Link id="navc5" href="/contact/" className={`NavLink ${currentPath === "/contact/" ? "active" : ""}`} onClick={handleLinkClick}>Contact</Link>
-          
+          <Link
+            id="navc3"
+            href="/productions/"
+            className={`NavLink ${
+              currentPath === "/productions/" ? "active" : ""
+            }`}
+            onClick={handleLinkClick}
+          >
+            Productions
+          </Link>
+          <Link
+            id="navc4"
+            href="/buzz/"
+            className={`NavLink ${currentPath === "/news/" ? "active" : ""}`}
+            onClick={handleLinkClick}
+          >
+            Buzz
+          </Link>
+          <Link
+            id="navc5"
+            href="/contact/"
+            className={`NavLink ${currentPath === "/contact/" ? "active" : ""}`}
+            onClick={handleLinkClick}
+          >
+            Contact
+          </Link>
+
           <a
             className=""
             href="https://visitor.r20.constantcontact.com/d.jsp?llr=csi9ozbab&p=oi&m=csi9ozbab&sit=zaxq5c9bb&f=56937576-3074-4bab-becd-a2b8ce970e8b"
@@ -93,3 +156,4 @@ export default function Navigation() {
     </nav>
   );
 }
+
